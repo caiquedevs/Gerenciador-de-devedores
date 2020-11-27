27c50a47da645e60ce96f23d806fc6d042db4194
caiquedevs <caique.lourenco@modalgr.com.br>
2020-11-27 14:58:48 -0300

    Criou Dashboard Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Provider } from 'react-redux';
+import { ToastContainer, Flip } from 'react-toastify';
+import DashboardPage from './pages/DashboardPage';
+import GlobalStyle from './styles';
+import store from './store/index.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <GlobalStyle />
+      <ToastContainer
+        transition={Flip}
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        className="toast-container"
+      />
+
+      <DashboardPage />
+    </Provider>
   );
 }
 
